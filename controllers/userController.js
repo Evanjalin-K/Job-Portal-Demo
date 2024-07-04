020b27db3cb956d1800607710da6f0f780034053
Evanjalin-K <jojaev510@gmail.com>
2024-07-04 11:07:05 +0530

    Query

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,26 @@
 // Define the controller for user
 const User = require('../models/user')
+const mongoose = require('mongoose')
 const userController = {
 
     getAllUsers: async(req, res) => {
 
         try {
+
+            const { email } = req.query;
+            if(email) {
+                //find the user by email
+
+                const user = await User.findOne({email});
+
+                if(!user){
+                    return res.status(404).send({ message: "User not found"})
+                }
+                return res.status(200).json(user)
+            }
+           // If no query
             //find({}, matching query, {projection})
-            const users = await User.find({}, {_id: 0, __v: 0, password: 0});
+            const users = await User.find({});
             res.status(200).json(users);
         } catch (error) 
         {
@@ -44,7 +58,26 @@ const userController = {
             res.status(500).send({ message: error.message});
             
         }
+      },
+      getUserById: async (req, res) => {
+          try {
+              const userId = req.params.id;
+      
+              if (!mongoose.Types.ObjectId.isValid(userId)) {
+                  return res.status(404).send({ message: 'User not found' });
+              }
+      
+              const foundUser = await User.findById(userId);
+      
+              if (!foundUser) {
+                  return res.status(404).send({ message: 'User not found' });
+              }
+      
+              res.status(200).json(foundUser);
+          } catch (error) {
+              console.error('Error finding user:', error);
+              res.status(500).send({ message: 'Internal server error' });
+          }
       }
-}
-
+    }      
 module.exports = userController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,10 @@ const userController = require('../controllers/userController')
 userRouter.get('/', userController.getAllUsers);
 userRouter.post('/', userController.register);
 
+//url params
+userRouter.get('/:id', userController.getUserById); 
+
+//query params
+
 
 module.exports = userRouter;
